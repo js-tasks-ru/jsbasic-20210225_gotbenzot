801f2313a630f32843539998ea01b6b0f9b83e1f
Bogdan <gotbenzot@gmail.com>
2021-03-06 16:34:34 +0300

    Выполнил задание Функция sumSalary(salaries), Зотов Богдан

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalaries = 0;
+  for(let key in salaries) {
+    if(typeof salaries[key] == 'number' && isFinite(salaries[key])) {
+        sumSalaries += salaries[key]; 
+    }  
+  }
+ return sumSalaries; 
 }
