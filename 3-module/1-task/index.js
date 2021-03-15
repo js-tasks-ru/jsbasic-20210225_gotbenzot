7ae18b2e814d8a4fd99e0180eef04d1db5825f1c
Bogdan <gotbenzot@gmail.com>
2021-03-15 23:00:29 +0300

    Исправил 5ое задание 3го модуля - "Найти min/max в строке"

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,5 @@
 function namify(users) {
+  //return users.map(item => item.name);
+  let [vasya,petya,masha] = users;
   return users.map(item => item.name);
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,10 +1,14 @@
 function getMinMax(str) {
-  let numbersOnly = str
+  const numbersOnly = str
   .split(',').join(' ').split(' ')
   .filter(item => Number(item));
-  let result = {
-    min: Math.min(...numbersOnly),
-    max: Math.max(...numbersOnly),
-  }
-  return result;
+  // let result = {
+  //   min: Math.min(...numbersOnly),
+  //   max: Math.max(...numbersOnly),
+  // }
+  //return result;
+  return {
+       min: Math.min(...numbersOnly),
+       max: Math.max(...numbersOnly),
+     }
 }
