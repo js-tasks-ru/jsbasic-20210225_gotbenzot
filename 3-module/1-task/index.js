b29f8560d55a92e1d49f666a6bea3456f8e11896
Bogdan <gotbenzot@gmail.com>
2021-03-16 12:34:02 +0300

    Решил через деструктуризацию объекта задание "Трансформировать в массив имён"

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,5 +1,5 @@
 function namify(users) {
   //return users.map(item => item.name);
-  let [vasya,petya,masha] = users;
-  return users.map(item => item.name);
+
+  return users.map(({name}) => name); // деструктурузировал объект
 }
